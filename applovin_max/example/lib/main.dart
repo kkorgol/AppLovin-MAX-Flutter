11f50e707b86a43a1f984c2e69a3429c39162f16
Konrad Korgol <konrad.korgol@gmail.com>
2022-10-23 14:57:03 +0200

    banners ad as widget issue #32

--- applovin_max/example/lib/main.dart
+++ applovin_max/example/lib/main.dart
@@ -3,11 +3,32 @@ import 'dart:io' show Platform;
 import 'dart:math';
 
 import 'package:applovin_max/applovin_max.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase/firebase_options.dart';
 
 enum AdLoadState { notLoaded, loading, loaded }
 
-void main() {
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  //await setupFlutterNotifications();
+  //showFlutterNotification(message);
+
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  print('Handling a background message ${message.messageId}');
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
